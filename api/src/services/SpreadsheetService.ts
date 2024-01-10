fe195bd02530ed873eb65bdd41cdbb4ba8f774db
Juan Resch <55303897+juan-resch@users.noreply.github.com>
2024-01-10 13:10:22 -0300

    Finalizando...

--- api/src/services/SpreadsheetService.ts
+++ api/src/services/SpreadsheetService.ts
@@ -50,6 +50,16 @@ export default class SpreadsheetService {
       throw error;
     }
   }
+
+  /*
+  Fiquei um pouco confuso com a periodicidade das assinaturas e como exatamente 
+  calcular MMR de Churn Rate de forma correta.
+
+  Diante algumas dúvidas eu optei por diluiur as assinaturas anuais em mensais 
+  (dividindo por 12) antes de calcular o MMR e Churn Rate e não tive sucesso.
+
+  então deixei apenas assinaturas mensais.
+*/
   static processSpreadsheetChartDataMonthly(data: SpreadsheetData[]) {
     data.sort((a, b) => new Date(a.startDate).getTime() - new Date(b.startDate).getTime());
 
